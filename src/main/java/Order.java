51df763bdeffe24ea23cb62e004013015858d59c
nasta889 <161520096+nasta889@users.noreply.github.com>
2024-11-20 14:36:59 +0200

    Update Order.java

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,2 +1,35 @@
 package PACKAGE_NAME;public class Order {
 }
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private static int nextOrderId = 1; // Генератор уникальных ID для заказов
+    private int orderId;
+    private List<Product> products;
+    private double totalPrice;
+
+    public Order() {
+        this.orderId = nextOrderId++;
+        this.products = new ArrayList<>();
+        this.totalPrice = 0.0;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+        totalPrice += product.getPrice();
+    }
+
+    public void displayOrderDetails() {
+        System.out.println("\n=== Детали заказа #" + orderId + " ===");
+        for (Product product : products) {
+            System.out.println(product);
+        }
+        System.out.println("Общая стоимость: " + totalPrice + " грн");
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public int get
